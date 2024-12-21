f02363a470c2e7e6f751277864421fca6c48c34d
savage-el <ellapusssss12@gmail.com>
2024-12-21 09:51:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDetails(id) {
+    const details = document.getElementById(id);
+    if (details.style.display === "block") {
+        details.style.display = "none";
+    } else {
+        details.style.display = "block";
+    }
+}
